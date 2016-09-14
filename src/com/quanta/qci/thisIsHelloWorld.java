6a016942e878a39531caf45b18bad32b7ffa4d7c
Shyang.Yang <Shyang.Yang@quantatw.com>
2016-09-14 13:44:13 +0800

    增加註解

--- src/com/quanta/qci/thisIsHelloWorld.java
+++ src/com/quanta/qci/thisIsHelloWorld.java
@@ -9,12 +9,25 @@ public class thisIsHelloWorld {
 		System.out.println(_thisHelloWorld.HelloName());
 		System.out.println(_thisHelloWorld.HelloName("Penny"));
 	}
+	/**
+	 * This is simple HelloWorld
+	 * @return
+	 */
 	public String Hello1(){
 		return "Hello World";
 	}
+	/**
+	 * This is simple HelloWorld with Shyang
+	 * @return
+	 */
 	public String HelloName(){
 		return "Hello World Shyang";
 	}
+	/**
+	 * This is simple HelloWorld with user input.
+	 * @param name user input
+	 * @return
+	 */
 	public String HelloName(String name){
 		return "Hello World "+name;
 	}
